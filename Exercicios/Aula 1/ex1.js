57fe1a38d4cf0f76f3eadd3b19c1b5b8f6130a09
Gabriel Machado <glemosmachado76@gmail.com>
2025-03-31 23:13:01 -0300

    first ts exercise

---
+++ Exercicios/Aula 1/ex1.js
@@ -0,0 +1,19 @@
+/**
+ * Exercise 1 - Easy
+ * Date: 31/03/2024
+ * Exercise Details:
+ *
+ *
+ * Write a short Typescript program that declares two variables:
+    One using implicit type inference (for example, assign a number or string without a type annotation).
+    One using explicit type annotation (for example, explicitly declare the type as number or string).
+    Then log both variables to the console.
+ */
+//Implicit
+var firstName = "Gabriel";
+var age = 27;
+//Explicit
+var isAlive = true;
+var height = 1.84;
+console.log("My name is:", firstName, ", I'm ", age, " years old!");
+console.log("Alive?", isAlive, "my height is:", height);
---
+++ Exercicios/Aula 1/ex1.ts
@@ -0,0 +1,21 @@
+/**
+ * Exercise 1 - Easy
+ * Date: 31/03/2024
+ * Exercise Details: 
+ * 
+ * 
+ * Write a short Typescript program that declares two variables:
+    One using implicit type inference (for example, assign a number or string without a type annotation).
+    One using explicit type annotation (for example, explicitly declare the type as number or string).
+    Then log both variables to the console.
+ */
+
+    //Implicit
+    let firstName         = "Gabriel"
+    let age               = 27
+    //Explicit
+    let isAlive: boolean = true
+    let height: number    = 1.84
+
+    console.log("My name is:", firstName, ", I'm ", age, " years old!");
+    console.log("Alive?", isAlive, "my height is:", height);
